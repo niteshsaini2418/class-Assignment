7eb8b2f6f3e3d01fdb37b1642e761ad98a4de1cc
niteshsaini2418 <49919006+niteshsaini2418@users.noreply.github.com>
2019-11-14 11:42:01 +0530

    assignment

---
+++ reverseLevelPrinting.java
@@ -0,0 +1,94 @@
+import java.util.*;
+public class reverseLevelPrinting
+{
+    Node root;
+    class Node
+    {
+        int data;
+        Node right,left;
+        Node(int data)
+        {
+            this.data=data;
+            right=left=null;
+        }
+    }
+    void insert(int data)
+    {
+        root=insertRec(root,data);
+    }
+    Node insertRec(Node root,int data)
+    {
+        if(root==null)
+        {
+            root=new Node(data);
+            return root;
+        }
+        if(data<root.data)
+        {
+            root.left=insertRec(root.left, data);
+        }
+        else if(data>root.data)
+        {
+            root.right=insertRec(root.right, data);
+        }
+        return root;
+    }
+    int height(Node root)
+    {
+        int c=0,c1=0;
+        if(root==null)
+        {
+            return 0;
+        }
+        if(root!=null)
+        {
+            c=height(root.left);
+            c++;
+            c1=height(root.right);
+            c1++;
+        }
+        if(c>c1)
+			return c;
+		else
+		    return c1;
+
+    }
+    void print(Node root)
+    {
+        int height=height(root);
+        for(int i=height;i>=1;i--)
+        {
+        reverseLevelprint(root,i); 
+        }
+
+    }
+    void reverseLevelprint(Node root,int level)
+    {
+        if(root==null)
+        return;
+        if(level==1)
+        System.out.print(root.data+" ");
+        else
+        reverseLevelprint(root.right,level-1);
+        reverseLevelprint(root.left,level-1);
+
+
+    }
+
+    public static void main(String args[])
+    {
+        reverseLevelPrinting tree=new reverseLevelPrinting();
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number of nodes");
+        int num = sc.nextInt();
+        System.out.println("Enter nodes");
+        
+        for(int i=0;i<num;i++)
+        {
+            tree.insert(sc.nextInt());
+        }
+        tree.print(tree.root);
+     
+    }
+
+}
